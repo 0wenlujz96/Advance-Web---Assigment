2be4b7f0cd7b26df0b3305eb812cf5c4f1f33255
0wenlujz96 <125528401+0wenlujz96@users.noreply.github.com>
2023-11-11 16:16:24 +1100

    Add files via upload

---
+++ Registration.php
@@ -0,0 +1,144 @@
+<?php
+require_once('config.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="myHeader">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <a href="Home.html">
+                        <img src="Logo/AlBums.png" class="img-fluid" alt="Logo" width="45" height="45">
+
+                    </a>
+                </div>
+                <div class="col-md-4 ">
+                    <form class="d-flex" role="search">
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                        <button class="btn btn-outline-success" type="submit">Search</button>
+                    </form>
+                </div>
+
+
+
+                <div class="col-md-4 ">
+                    <button class="btn btn-primary float-end" type="submit">Login</button>
+                   
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- header -->
+
+    <div class="myMenu">
+        <div class="container">
+            <nav class="navbar navbar-expand-lg bg-body-tertiary">
+                <div class="container-fluid">
+                  <a class="navbar-brand d-none" href="#">Navbar</a>
+                  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                  </button>
+                  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                      <li class="nav-item">
+                        <a class="nav-link text-white" href="Trending.html">Trending</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link text-white" href="Aboutus.html">About Us</a>
+                      </li>
+                      <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                          Categories
+                        </a>
+                        <ul class="dropdown-menu">
+                          <li><a class="dropdown-item" href="#">Action</a></li>
+                          <li><a class="dropdown-item" href="#">Another action</a></li>
+                          <li><hr class="dropdown-divider"></li>
+                          <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </nav>
+        </div>
+    </div>
+    <!-- menu -->
+
+    <div>
+      <?php
+        if(isset($_POST["created"])) {
+          $fname = $_POST["fname"];
+          $lname = $_POST["lname"];
+          $uname = $_POST["uname"];
+          $email = $_POST["email"];
+          $password = $_POST["password"];
+
+          $sql = "INSERT INTO users(fname, lname, uname, email, password) VALUE(?,?,?,?,?)";
+          $stmtinsert = $conn->prepare($sql);
+          $result = $stmtinsert->execute([$fname, $lname, $uname, $email, $password]);  
+
+          if($result) {
+            echo '
+              <script>
+                window.location.href = "Home.html";
+                alert("Registration success")
+              </script>
+            ';
+          } else {
+            echo '
+              <script>
+                window.location.href = "Registration.php";
+                alert("Error, pls try again")
+              </script>
+            ';
+          }
+
+        }
+      ?>
+    </div>
+
+    <form action="Registration.php" method="post">
+      <div class="boxLogin py-4 text-white container">
+        <h1>Registration</h1>
+        <p>Fill up with correct values.</p>
+        <div class="row">
+          <div class="col-ms-2">
+            <label for="fname"><b>First Name</b></label>   
+            <input class="form-control" type="text" name="fname" required>   
+
+            <label for="lname"><b>Last Name</b></label>   
+            <input class="form-control" type="text" name="lname" required>  
+
+            <label for="email"><b>Email</b></label>   
+            <input class="form-control" type="text" name="email" required>  
+
+            <label for="uname"><b>User Name</b></label>   
+            <input class="form-control" type="text" name="uname" required>  
+
+            <label for="password"><b>Password</b></label>   
+            <input class="form-control" type="password" name="password" required>  
+            <br>
+            <input class="form-control btn btn-primary" type="submit" name="created" value="Submit">
+          </div>
+        </div>
+        
+      </div>
+    </form>
+    
+
+    
+  </div>
+    
+</body>
+</html>
